601c2fa5bcbb9fa9887ab0a09f5e53792fcf1983
Melisa Brulić <melisa.brulic@student.fmf.uni-lj.si>
2019-11-18 11:58:02 +0100

    g

--- izpiti/2019-11-18/kolokvij-2019-11-18.ml
+++ izpiti/2019-11-18/kolokvij-2019-11-18.ml
@@ -2,27 +2,53 @@
 (* 1. naloga: funkcije *)
 (* =================== *)
 
-let is_root () = failwith "dopolni me"
+let is_root x y = 
+    if x<0 
+    then false 
+    else x * x = y
 
-let pack3 () = failwith "dopolni me"
+let pack3 x y z = (x, y, z)
 
-let sum_if_not () = failwith "dopolni me"
+let rec sum_if_not predikat list = 
+    match list with
+    | [] -> 0
+    | x :: xs -> 
+        if not (predikat x)
+        then x + (sum_if_not predikat xs) 
+        else 
+        sum_if_not predikat xs
 
-let apply () = failwith "dopolni me"
+
+(*let rec apply list1 list2 =
+    match list1, list2 with 
+    | [], [] -> [[]]
+    | f :: fs, [] -> [[]]
+    | [], x :: xs -> [] :: (apply [] xs)
+    | f :: fs, x :: xs -> f x ::*)
+
+    
 
 (* ======================================= *)
 (* 2. naloga: podatkovni tipi in rekurzija *)
 (* ======================================= *)
 
-type vrsta_srecanja = DopolniMe
+type vrsta_srecanja = 
+    | Predavanja of int
+    | Vaje of int
 
-type srecanje = DopolniMe'
+type srecanje = {predmet: string; vrsta: vrsta_srecanja}
 
-let vaje () = failwith "dopolni me"
-let predavanje () = failwith "dopolni me"
+type urnik = Urnik of srecanje list list
 
-let urnik_profesor () = failwith "dopolni me"
+let vaje = {predmet = "Analiza 2a"; vrsta = Vaje(3)}
 
-let je_preobremenjen () = failwith "dopolni me"
+let predavanje = {predmet = "Programiranje 1"; vrsta = Predavanja(2)}
 
-let bogastvo () = failwith "dopolni me"
+let urnik_profesor = [[{predmet = "Analiza 2a"; vrsta = Vaje(2)}];[]; [{predmet = "Algebra 2"; vrsta = Predavanja(1) }]; [];[];[{predmet = "Programiranje 1"; vrsta = Vaje(1)}];[]]
+
+(*let je_preobremenjen Urnik(list) = 
+    match list with 
+    | [] -> false
+    | s :: ss -> *)
+
+let bogastvo () = ()
